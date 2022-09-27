d3d8432fe91ba5f909a04d8b020989db693d3770
Enrique Rodriguez <soraestafar@gmail.com>
2022-09-27 08:31:24 -0400

    NPC Movement

---
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/Ai.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4969b7873de4996b9970e2b720429136df3d29e0fbf077237b72439991191063
+size 585
---
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/GetPoint.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d9b269b712b271ff9a04274a165bbeac3f54bb4c09e5ae763161e0a0d4c9913
+size 1290
