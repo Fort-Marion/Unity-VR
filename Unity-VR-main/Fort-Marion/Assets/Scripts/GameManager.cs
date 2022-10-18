226e6f7a37540bd45fa30f282c9e76e6cdd60dfe
Ster <sterz.dev@gmail.com>
2022-10-18 09:55:06 -0400

    Add updated game texts and localization

---
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e20915d192ea31802d5143aa1525a5f5304a861a8afda127a6adf3d3e029d1c
+size 837
---
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/Localization/TextManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f27074e7cf01dbd68f01e1f8b5f5e83cf9b080e8e7a2df0d20c085efabb847a3
+size 1198
