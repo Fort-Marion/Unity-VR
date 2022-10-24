425af2a93491d0a8c8df6158e9ffa39c83775e97
pzou15 <84814375+pzou15@users.noreply.github.com>
2022-10-24 16:43:26 -0400

    Detect Controller Working with Simulated HMD
    detect controller updated to work with simulated HMD and player body. Added rigs and player body to prefab folder.

--- Unity-VR-main/Fort-Marion/Assets/Scripts/DetectController.cs
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/DetectController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f6d10c76d54b1889bcfdcbbf1046c7d0eecb535a7d7fd201f4105eedc1ab66a
-size 803
+oid sha256:46a4ab00ce465abd4a21994662c9a19c7c27f07cf16d00f63ccfb23b664adaba
+size 912
