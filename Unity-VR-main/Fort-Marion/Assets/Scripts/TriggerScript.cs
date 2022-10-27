13392a6fe27cb10eb871d1569534936c3ec3a610
Enrique Rodriguez <soraestafar@gmail.com>
2022-10-26 23:31:53 -0400

    Re-baked navmesh and fixed trigger system

--- Unity-VR-main/Fort-Marion/Assets/Scripts/TriggerScript.cs
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/TriggerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:264f5ae08a333d2996d775650453b7dd365818915f1cb09859bad056bdcd3e81
-size 729
+oid sha256:a51768bb33e470d1bd969fdce5f1d7dfc33796fba807268be38562d25600a99b
+size 607
