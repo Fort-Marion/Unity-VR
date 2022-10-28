4e8efe17f953b23dca6db0f7cea8dfe6d48d110d
Ster <sterz.dev@gmail.com>
2022-10-28 19:52:17 -0400

    Fix project building

--- Unity-VR-main/Fort-Marion/Assets/Scripts/GameManager.cs
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbaf5d6f4b04fd454a7e4a0e1357df5d710605f3308aac7a0a096c4174c4f83d
-size 827
+oid sha256:dac8c1fe38b8ecdc2eaae4f54077c92260a03777fc8d3ab8e03c744852364814
+size 801
