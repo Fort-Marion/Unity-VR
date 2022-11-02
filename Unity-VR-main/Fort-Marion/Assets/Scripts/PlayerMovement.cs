1a9ca02bd3584838c6aa023c822b09388931aa73
pzou15 <84814375+pzou15@users.noreply.github.com>
2022-11-01 23:38:38 -0400

    fixed player sliding, removed jump and air drag

--- Unity-VR-main/Fort-Marion/Assets/Scripts/PlayerMovement.cs
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,6 @@ public class PlayerMovement : MonoBehaviour
 
     public float groundDrag;
 
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    private bool canJump;
-
-    public KeyCode jumpKey = KeyCode.Space;
-
-    public float playerHeight;
-    public LayerMask Ground;
-    private bool grounded;
-
-
     public Transform orientation;
     public Rigidbody rb;
 
@@ -32,23 +20,14 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb.freezeRotation = true;
-        canJump = true;
+        rb.drag = groundDrag;
     }
 
     void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
         GetInput();
         SpeedControl();
 
-        if(grounded)
-        {
-            rb.drag = groundDrag;
-        }
-        else
-        {
-            rb.drag = 0;
-        }
     }
 
     void FixedUpdate()
@@ -61,26 +40,14 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jumpKey) && canJump && grounded)
-        {
-            canJump = false;
-            Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
+
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f , ForceMode.Force);
 
-        if (grounded)
-        {
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        }
-        else
-        {
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-        }
         
     }
 
@@ -94,18 +61,5 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
         }
     }
-
-    private void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-
-    private void ResetJump()
-    {
-        canJump = true;
-    }
-
 
 }
