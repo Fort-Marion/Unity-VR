b4acaeb8a1d7b577bcf80b286f44f700cabde72d
Erodz00 <soraestafar@gmail.com>
2022-11-03 11:52:27 -0400

    Checked Dev Build

---
+++ Unity-VR-main/Fort-Marion/Assets/Scripts/BridgeOpener.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeOpener : MonoBehaviour
+{
+    [SerializeField] private GameObject bridge;
+
+    private void OnTriggerEnter(Collider Spawner)
+    {
+    }
+    private void OnTriggerExit(Collider Spawner)
+    {
+        
+    }
+}
